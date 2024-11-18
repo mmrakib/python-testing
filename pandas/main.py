1e9bc8d493aa4db6ed56aaeb28212e2428d49727
mmrakib <mohammadmrakib@gmail.com>
2024-11-19 01:32:47 +1100

    First commit

---
+++ pandas/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = {'Name': ['John', 'Anna', 'Peter'],
+        'Age': [28, 24, 35],
+        'City': ['New York', 'London', 'Berlin']}
+df = pd.DataFrame(data)
+print(df)
